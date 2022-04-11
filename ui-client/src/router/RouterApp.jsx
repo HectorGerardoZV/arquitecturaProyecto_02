9d43ea88afdd340adc976bf726fb36c20ae97940
DianaLauraMN <dianaalauramn@gmail.com>
2022-04-10 21:57:59 -0700

    Pages alarms added

--- ui-client/src/router/RouterApp.jsx
+++ ui-client/src/router/RouterApp.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Login from "../pages/login/Login"
 import MasterPage from "../pages/materPage/MasterPage"
+import AlarmsPage from "../pages/alarmsPage/AlarmsPage"
+import NewAlarmPage from "../pages/newAlarmPage/NewAlarmPage"
 
 const RouterApp = () => {
   return (
@@ -8,7 +10,8 @@ const RouterApp = () => {
         <Routes>
             <Route path="/" element={<Login/>}/>
             <Route path="/admin" element={<MasterPage/>}>
-
+            <Route path="alarms" element={<AlarmsPage/>}/>
+            <Route path="newAlarm" element={<NewAlarmPage/>}/> 
             </Route>
         </Routes>
     </Router>
