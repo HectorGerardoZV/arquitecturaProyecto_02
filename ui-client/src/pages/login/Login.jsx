c6d6f3b6b5719ecfd3ba5e6e7cab352315349de8
DianaLauraMN <dianaalauramn@gmail.com>
2022-05-06 13:19:22 -0700

    Login created, logo-blue was replaced, LoginCard component was deleted.

--- ui-client/src/pages/login/Login.jsx
+++ ui-client/src/pages/login/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import "./Login.css"
+import LogoBlue from "../../img/logo-blue-login.svg"
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div className="login-area">
+      <form className="form-login height-form">
+
+        <div className="logo-login">
+          <img src={LogoBlue} alt="Logo invernadero" />
+        </div>
+
+        <div className="inputsForm">
+          <div className="inputForm input-height">
+            <label htmlFor="">Usuario</label>
+            <input type="text" />
+          </div>
+
+          <div className="inputForm input-height">
+            <label htmlFor="">Contraseña</label>
+            <input type="password" />
+          </div>
+
+        </div>
+        <div className="inputSubmitArea">
+          <input className="inputSubmit"
+            type="submit" value="Iniciar Sesión" />
+        </div>
+
+      </form>
+    </div>
   )
 }
 
