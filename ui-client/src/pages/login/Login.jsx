0f15e4991a7882ed8a25fabca4302017ffb07f51
DianaLauraMN <dianaalauramn@gmail.com>
2022-05-06 14:41:39 -0700

    Login was updated

--- ui-client/src/pages/login/Login.jsx
+++ ui-client/src/pages/login/Login.jsx
@@ -1,10 +1,34 @@
 import "./Login.css"
 import LogoBlue from "../../img/logo-blue-login.svg"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 
 const Login = () => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleOnChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleOnChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //navigate("/admin")
+    const user = {
+      email: email,
+      password: password
+    }
+  }
+
   return (
     <div className="login-area">
-      <form className="form-login height-form">
+      <form className="form-login height-form" onSubmit={handleSubmit}>
 
         <div className="logo-login">
           <img src={LogoBlue} alt="Logo invernadero" />
@@ -12,13 +36,13 @@ const Login = () => {
 
         <div className="inputsForm">
           <div className="inputForm input-height">
-            <label htmlFor="">Usuario</label>
-            <input type="text" />
+            <label htmlFor="">Email</label>
+            <input type="text" onChange={handleOnChangeEmail} value={email} />
           </div>
 
           <div className="inputForm input-height">
             <label htmlFor="">Contraseña</label>
-            <input type="password" />
+            <input type="password" onChange={handleOnChangePassword} value={password} />
           </div>
 
         </div>
